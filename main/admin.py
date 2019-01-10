2b8f45d4127244352961fba1464f257b0a2f86e0
ono5 <kuroneko_head_lord@yahoo.co.jp>
2019-01-10 21:38:24 +0900

    Custom search

--- main/admin.py
+++ main/admin.py
@@ -8,4 +8,10 @@ class BlogAdmin(admin.ModelAdmin):
     list_display = ('title', 'date_created', 'last_modified', 'is_draft')
     list_filter = ('is_draft',)
 
+    def get_ordering(self, request):
+        if request.user.is_superuser:
+            return ('title', '-date_created')
+        return ('title',)
+
+
 admin.site.register(Blog, BlogAdmin)
