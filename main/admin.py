b41c2f6b354787ad2f095e105747b887dfdc9d16
ono5 <kuroneko_head_lord@yahoo.co.jp>
2019-01-10 21:21:00 +0900

    List display

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-
 from main.models import Blog
 
 
-admin.site.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'date_created', 'last_modified', 'is_draft')
+
+
+admin.site.register(Blog, BlogAdmin)
