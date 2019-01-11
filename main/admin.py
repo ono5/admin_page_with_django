0b80137bc7ba088c3dd9a7e4be6af02322f6ca36
ono5 <kuroneko_head_lord@yahoo.co.jp>
2019-01-12 05:40:03 +0900

    Custom admin changes list action

--- main/admin.py
+++ main/admin.py
@@ -10,11 +10,18 @@ class BlogAdmin(admin.ModelAdmin):
     search_fields = ('title', )
     prepopulated_fields = {'slug': ('title', )}
     list_per_page = 50
+    actions = ('set_blogs_to_published', )
 
     def get_ordering(self, request):
         if request.user.is_superuser:
             return ('title', '-date_created')
         return ('title',)
+
+    def set_blogs_to_published(self, request, queryset):
+        """Set Action behave"""
+        count = queryset.update(is_draft=False)
+        self.message_user(request, '{} blogs have been published successfully.'.format(count))
+    set_blogs_to_published.short_description = 'Mark selected blog as published'
 
 
 admin.site.register(Blog, BlogAdmin)
