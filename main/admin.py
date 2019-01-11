bc7712640fb8031268006d277ce1c38cb89c032f
ono5 <kuroneko_head_lord@yahoo.co.jp>
2019-01-12 06:06:58 +0900

    Model properties in the admin

--- main/admin.py
+++ main/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
+from django.utils import timezone
 
 from main.models import Blog
 
 
 class BlogAdmin(admin.ModelAdmin):
 
-    list_display = ('title', 'date_created', 'last_modified', 'is_draft')
+    list_display = ('title', 'date_created', 'last_modified', 'is_draft', 'days_since_creation')
     list_filter = ('is_draft', 'date_created')
     search_fields = ('title', )
     prepopulated_fields = {'slug': ('title', )}
@@ -21,6 +22,12 @@ class BlogAdmin(admin.ModelAdmin):
             'description': 'Opstions to configure blog creation',
         })
     )
+
+    def days_since_creation(self, blog):
+        """diff date to show on the list"""
+        diff = timezone.now() - blog.date_created
+        return diff.days
+    days_since_creation.short_description = 'DAY ACTIVE'
 
     def get_ordering(self, request):
         if request.user.is_superuser:
